8647461e26d9497a19324f28aa1bdc29e7c88276
g9308370 <g9308370@hotmail.com>
2015-07-07 00:44:04 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -3,11 +3,13 @@
 	$link = mysql_pconnect("localhost", "staff", "0935820227");
 	mysql_select_db("staff",$link) or die("無法選擇資料庫");
 	mysql_query("SET NAMES 'utf8'");
-	$d="select max(id) from staff";
-	$e=mysql_query($d,$link);
-	$f=mysql_result($e,0);
-	echo $f;
-	$sql = "insert into answer (TopicNo,StartTime) values('23','2014-07-07')" or die("insert error");
+	
+	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T','$TopicNo','$date_time')" or die("insert error");
+ 
+	
+	
 	mysql_query($sql,$link);
-	echo "hello";
+	
+	 ?>
+</p>
 ?>
