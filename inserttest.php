323eb5a7deb0fc847603979b06a3fe4f03544a92
g9308370 <g9308370@hotmail.com>
2015-07-07 00:57:53 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -5,13 +5,8 @@
 	 $link = mysql_pconnect("localhost", "staff", "0935820227");
 	 mysql_select_db("staff",$link) or die("無法選擇資料庫");
 	 mysql_query("SET NAMES 'utf8'");
-	
 	$sql = "insert into answer (TopicNo,StartTime) values('23','1')" or die("insert error");
- 
-	
-	
 	mysql_query($sql,$link);
-
-	 ?>
+?>
 
 
