8a439e89b05df46e23f520b97aaa1cb4f5910eb6
g9308370 <g9308370@hotmail.com>
2015-07-07 00:38:00 +0800

    Update 20140819.php

--- 20140819.php
+++ 20140819.php
@@ -56,23 +56,6 @@
 
 	 ?>
 </p>
-<p>
-    <? 
-	
-	//第二個存入data2的資料庫
-	if ((isset($_POST["TopicNo"])))
-	{
-	 $link = mysql_pconnect("localhost", "staff", "0935820227");
-	 mysql_select_db("staff",$link) or die("無法選擇資料庫");
-	 mysql_query("SET NAMES 'utf8'");
-	
-	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T','$TopicNo','$date_time')" or die("insert error");
- 
-	
-	
-	mysql_query($sql,$link);
-	}
-	 ?>
-</p>
+
 </body>
 </html>
