ea7e9c33352505ef45a2f02af1240aa2e903e5ea
g9308370 <g9308370@hotmail.com>
2015-07-07 14:40:40 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -3,6 +3,7 @@
 <p>題序:
 <input type="text" name="TopicNo"/>
 <input name="btn" type="submit"  id="btn"/>
+</html>
 <?php
 	//第二個存入data2的資料庫
 	date_default_timezone_set('Asia/Taipei');   
@@ -13,4 +14,3 @@
 	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T',"$_POST[TopicNo]",'$date_time')" or die("insert error");
 	mysql_query($sql,$link);
 ?>
-</html>
