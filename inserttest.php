6e2a00646599c5676e98d2d5462520b846449d7c
g9308370 <g9308370@hotmail.com>
2015-07-07 14:29:29 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -11,7 +11,7 @@
 	$link = mysql_pconnect("localhost", "staff", "0935820227");
 	mysql_select_db("staff",$link) or die("無法選擇資料庫");
 	mysql_query("SET NAMES 'utf8'");
-	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T','$TopicNo','$date_time')" or die("insert error");
+	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T',"$_POST[TopicNo]",'$date_time')" or die("insert error");
 	mysql_query($sql,$link);
 ?>
 </html>
