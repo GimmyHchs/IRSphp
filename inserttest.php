4eff4091f72450dc3a85d62c0fb3d30f296051fb
g9308370 <g9308370@hotmail.com>
2015-07-07 14:39:24 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -3,5 +3,14 @@
 <p>題序:
 <input type="text" name="TopicNo"/>
 <input name="btn" type="submit"  id="btn"/>
-
+<?php
+	//第二個存入data2的資料庫
+	date_default_timezone_set('Asia/Taipei');   
+	$date_time = date("Y-m-d H:i:s");
+	$link = mysql_pconnect("localhost", "staff", "0935820227");
+	mysql_select_db("staff",$link) or die("無法選擇資料庫");
+	mysql_query("SET NAMES 'utf8'");
+	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T',"$_POST[TopicNo]",'$date_time')" or die("insert error");
+	mysql_query($sql,$link);
+?>
 </html>
