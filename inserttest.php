c4e72ef5aa263f3ff239e186e83039a9dac82b4d
g9308370 <g9308370@hotmail.com>
2015-07-07 14:05:52 +0800

    Update inserttest.php

--- inserttest.php
+++ inserttest.php
@@ -2,16 +2,9 @@
 	//第二個存入data2的資料庫
 	date_default_timezone_set('Asia/Taipei');   
 	$date_time = date("Y-m-d H:i:s");
-
 	$link = mysql_pconnect("localhost", "staff", "0935820227");
-
-	 mysql_select_db("staff",$link) or die("無法選擇資料庫");
-	 mysql_query("SET NAMES 'utf8'");
-	
-	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T','3','$date_time')" or die("insert error");
- 
-	
-	
+	mysql_select_db("staff",$link) or die("無法選擇資料庫");
+	mysql_query("SET NAMES 'utf8'");
+	$sql = "insert into openanswer (IfOpenanswer,TopicNo,time) values('T','3','$date_time')" or die("insert error")
 	mysql_query($sql,$link);
-	
 ?>
